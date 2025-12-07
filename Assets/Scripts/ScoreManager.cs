37e50648b57f28a4c9bf2b5a264945fd5db4fa2c
ManuelCarrasco28 <josemanuelcarrascomillan@gmail.com>
2025-12-07 16:11:07 -0500

    Actualización Coins Nunca se destruyen

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,28 +1,93 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
+
     public Text scoreText;
 
-    int score = 0;
+    private int score = 0;
 
-    private void Awake()
+    void Awake()
     {
+        // Singleton para evitar duplicados
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // Escuchar cuando se carga una escena nueva
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    // Start is called before the first frame update
+    void OnDestroy()
+    {
+        // Por seguridad, desuscribir
+        if (instance == this)
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+
     void Start()
     {
-        scoreText.text = score.ToString() + " Coins";
+        BuscarTextoUI();
+        ActualizarUI();
     }
 
-    public void AddPoint() {
-        score += 1;
-        scoreText.text = score.ToString() + " Coins";
+    // Se llama cada vez que se carga una escena
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Si volvemos al menú, reiniciamos el score
+        if (scene.name == "MENU")
+        {
+            score = 0;
+        }
+
+        // Volvemos a buscar el texto de UI en la nueva escena
+        BuscarTextoUI();
+        ActualizarUI();
+    }
+
+    void BuscarTextoUI()
+    {
+        // Si ya tiene referencia, no hace falta buscar
+        if (scoreText != null) return;
+
+        // Busca SOLO el texto cuyo GameObject se llame "score"
+        Text[] textos = FindObjectsOfType<Text>();
+        foreach (Text t in textos)
+        {
+            if (t.gameObject.name == "score")
+            {
+                scoreText = t;
+                break;
+            }
+        }
+    }
+
+    void ActualizarUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = score + " Coins";
+        }
+    }
+
+    public void AddPoint()
+    {
+        score++;
+        ActualizarUI();
+    }
+
+    public int GetScore()
+    {
+        return score;
     }
 }
